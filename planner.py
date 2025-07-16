8acd1e86e95d8d2ba2fd617d056d641e935f4345
legorockband <115738168+legorockband@users.noreply.github.com>
2025-07-15 20:42:01 -0700

    created cell finding and some made parameters for DFS

--- planner.py
+++ planner.py
@@ -9,7 +9,6 @@ Examine the command line arguments and run either uniform cost or DFS on the tex
 
 import sys
 
-
 def main():
     if len(sys.argv) != 3:
         print("Usage: python3 planner.py [search algorithm] [.txt file]")
@@ -24,20 +23,23 @@ def main():
         print(f"Usage: Make sure [{sys.argv[2]}] is in .txt format")
         sys.exit(1)
 
+    ## Create Grid from txt file
     grid = text_parsing(input_text_file)
 
+    start_position, dirty_cells = find_important_cells(grid)
+
+    ## Check what algorithm you want to use 
     if (algo_to_run == "uniform_cost"):
-        uniform_cost()
+        #uniform_cost(grid, start_position)
         return 0
     
     elif (algo_to_run == "depth_first"):
-        depth_first()
+        depth_first(grid, start_position)
         return 0
 
     print(f"Usage: Wrong alogithm ran [{algo_to_run}], should be either uniform_cost or depth_first")
 
     return 0
-
 
 """
 Args:
@@ -45,10 +47,11 @@ Args:
 
 Output:
   -2D array of empty, blocked, dirty, and starting cells 
-   - `_` for empty
-   - `#` for blocked
-   - `*` for dirty
-   - `@` for robot start (placed randomly in a non-blocked, non-dirty cell)
+    - `_` for empty
+    - `#` for blocked
+    - `*` for dirty
+    - `@` for robot start (placed randomly in a non-blocked, non-dirty cell)
+  - Starting row and column 
 """
 
 def text_parsing(text_file):
@@ -57,26 +60,50 @@ def text_parsing(text_file):
         rows = int(file.readline().strip())
 
         world_grid = []
+
+        ## Go through each line in a row and add it to a list
         for _ in range(rows):
             grid_line = file.readline().strip()
             world_grid.append(grid_line)
+            
     file.close()
 
     return world_grid
 
+def find_important_cells(grid):
+    dirty_cells = []
+    
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            ## Find the start cell
+            if grid[row][col] == '@':
+                starting_pos = (row,col)
+            
+            ## Find dirty cell 
+            elif grid[row][col] == '*':
+                dirty_cells.append((row,col))
 
-def uniform_cost():
+    return starting_pos, dirty_cells
+
+
+def depth_first(grid, starting_pos):
+    row = starting_pos[0]
+    col = starting_pos[1]
+
+    ## Create a stack with the starting position in there
+    stack = [starting_pos]
+
+    
 
 
     return 0
 
-def depth_first():
-
-
-
+def uniform_cost(grid, rows, columns):
 
 
     return 0
+
+
 
 if __name__ == "__main__":
     main()
